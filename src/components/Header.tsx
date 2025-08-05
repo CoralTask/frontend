7c685fd2aeefe197f5e0f2f80d4f19e0d54bf368
NamDoHyeon2 <skaehgus119@gmail.com>
2025-08-05 17:41:06 +0900

    feat : 메인화면 디자인 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 w-full bg-white shadow-md z-10">
+      <div className="w-full px-6 py-3 flex justify-between items-center">
+        <h1 className="text-gray-900 text-xl font-bold">🌊 CORALTASK</h1>
+            <div className="flex gap-4">
+            <button className="bg-white text-primary-300 px-12 py-3 rounded-xl font-pretendard text-base">
+                로그인
+            </button>
+            <button className="bg-primary-300 text-white px-12 py-3 rounded-xl font-pretendard text-base">
+                회원가입
+            </button>
+            </div>
+      </div>
+    </header>
+  )
+}
+export default Header
