abb838634e6ada0520d3328b2598d740d0051579
NamDoHyeon2 <skaehgus119@gmail.com>
2025-08-06 15:02:31 +0900

    feat : 로그인 라우터 연결

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+
 const Header = () => {
+  const navigate = useNavigate()
+  
   return (
     <header className="absolute top-0 left-0 w-full bg-white shadow-md z-10">
       <div className="w-full px-6 py-3 flex justify-between items-center">
         <h1 className="text-gray-900 text-xl font-bold">🌊 CORALTASK</h1>
             <div className="flex gap-4">
-            <button className="bg-white text-primary-300 px-12 py-3 rounded-xl font-pretendard text-base">
+            <button 
+              onClick={() => navigate('/login')}
+              className="bg-white text-primary-300 px-12 py-3 rounded-xl font-pretendard text-base">
                 로그인
             </button>
             <button className="bg-primary-300 text-white px-12 py-3 rounded-xl font-pretendard text-base">
