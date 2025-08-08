1cf4325809d51c160c175b49b8aaf14ca51716a3
NamDoHyeon2 <skaehgus119@gmail.com>
2025-08-08 16:17:56 +0900

    feat : 로그인 디자인 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,10 @@
 import LoginForm from '../components/LoginForm'
+import Header from '../components/Header'
 
 const LoginPage = () => {
   return (
     <div>
-      <h1>로그인</h1>
+      <Header/>
       <LoginForm />
     </div>
   )
