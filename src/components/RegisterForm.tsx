f5d13bf71112227848a9f2f351187dd4a93ab058
NamDoHyeon2 <skaehgus119@gmail.com>
2025-08-11 16:48:49 +0900

    feat : 회원가입 라우터 연결

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,7 @@
+const RegisterForm = () => {
+    return(
+        <h1></h1>
+    )
+}
+
+export default RegisterForm;
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import Register from '../components/RegisterForm'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <Header/>
+      <Register />
+    </div>
+  )
+}
+
+export default RegisterPage
