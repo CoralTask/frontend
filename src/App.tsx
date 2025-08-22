a5509336a231a36583d91217e83db5ef69997718
NamDoHyeon2 <skaehgus119@gmail.com>
2025-08-22 16:05:22 +0900

    feat : 회원가입 후 프로필 작성 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import StartPage from './pages/StartPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import MainPage from "./pages/MainPage"
+import ProfilePage from "./pages/ProfilePage"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/main" element={<MainPage/>}/>
+        <Route path="/profile" element ={<ProfilePage/>}/>
       </Routes>
     </div>
   )
---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import Profile from '../components/ProfileForm'
+import Header from '../components/Header'
+
+const ProfilePage = () => {
+    return (
+        <div>  
+            <Header/>
+            <Profile/>
+        </div>
+    )
+}
+
+export default ProfilePage;
